0b89885dec697c5bab67d637b96463fa45afd891
adamgareb <92087000+adamgareb@users.noreply.github.com>
2022-09-12 13:03:20 +0200

    Add files via upload

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Snake
+    {
+        //I have no idea what this is
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Snake()
+        { //Width and height you get it
+            Width = 100;
+            Height = 28;
+        }
+
+        //draw the canvas?
+        public void drawCanvas()
+        {
+            Console.Clear();
+            //dessa for loops är det som gör boxen. t.ex. det här är top raden
+            for(int i = 0; i < Width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("-");
+            }
+            //och detta bottom delen osv
+            for(int i = 0; i < Width; i++)
+            {
+                Console.SetCursorPosition(i, Height);
+                Console.Write("/");
+            }
+
+            for(int i = 0; i < Height; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("-");
+            }
+
+            for(int i = 0; i < Height; i++)
+            {
+                Console.SetCursorPosition(Width, i);
+                Console.Write("-");
+            }
+        }
+
+    }
+}
---
+++ Snake/Snake/habla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool finished = false;
+            Snake canvas = new Snake();
+
+            while (!finished)
+            {
+                canvas.drawCanvas();
+                Console.Read();
+            }
+
+        }
+    }
+}
